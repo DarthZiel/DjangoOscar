3a7b7a86a35eb82c4982cebd7279a1ccb4757052
DarthZiel <Studerzhan@gmail.com>
2024-03-05 15:00:18 +0500

    init project

---
+++ frobshop/frobshop/urls.py
@@ -0,0 +1,36 @@
+from django.apps import apps
+from django.urls import include, path
+from django.contrib import admin
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+    # The Django admin is not officially supported; expect breakage.
+    # Nonetheless, it's often useful for debugging.
+
+    path('admin/', admin.site.urls),
+
+    path('', include(apps.get_app_config('oscar').urls[0])),
+    path("api/v1/", include("oscarapi.urls")),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+
