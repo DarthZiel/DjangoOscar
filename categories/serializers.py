a6d8248ef425612dfbe245b72667933079fe3a9b
DarthZiel <Studerzhan@gmail.com>
2024-05-22 18:46:58 +0500

    repair subvategories image url and products images

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,13 +1,19 @@
-from oscar.apps.catalogue.models import Category, Product
-from oscarapi.serializers.product import BaseCategorySerializer, BaseProductSerializer
 from rest_framework import serializers
-
+from oscar.apps.catalogue.models import Category, Product
+from oscarapi.serializers.product import BaseCategorySerializer, BaseProductSerializer, ProductImageSerializer
 
 class ChildCategorySerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        exclude = ('path', 'depth')
+        fields = '__all__'
 
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image and request:
+            return request.build_absolute_uri(obj.image.url)
+        return None
 
 class CustomCategorySerializer(BaseCategorySerializer):
     children = serializers.SerializerMethodField()
@@ -19,7 +25,8 @@ class CustomCategorySerializer(BaseCategorySerializer):
             request = self.context.get('request')
             children_data = []
             for child_category in child_categories:
-                child_data = ChildCategorySerializer(child_category).data
+                child_serializer = ChildCategorySerializer(child_category, context={'request': request})
+                child_data = child_serializer.data
                 if request:
                     hostname = request.get_host()
                     url = f'http://{hostname}/api/mycategories/{child_category.slug}'
@@ -27,6 +34,7 @@ class CustomCategorySerializer(BaseCategorySerializer):
                 children_data.append(child_data)
             return children_data
         return None
+
     def get_products(self, instance):
         products = instance.product_set.all()
         product_serializer = CustomBaseProductSerializer(products, many=True, context=self.context)
@@ -36,8 +44,8 @@ class CustomCategorySerializer(BaseCategorySerializer):
         model = Category
         exclude = ('path', 'depth')
 
-
 class CustomBaseProductSerializer(BaseProductSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
     class Meta:
         model = Product
         fields = '__all__'
--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,10 @@
-from oscarapi.utils.categories import find_from_full_slug
+from django.http import Http404
 from rest_framework import status
-from rest_framework.generics import ListAPIView
-from oscarapi.views.product import CategoryList
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from oscarapi.views.product import CategoryList
 from oscar.apps.catalogue.models import Category, Product
 from .serializers import CustomCategorySerializer, CustomBaseProductSerializer
-
-from django.http import Http404
-from rest_framework.generics import get_object_or_404
 
 class CustomCategoryList(CategoryList):
 
@@ -19,12 +16,9 @@ class CustomCategoryList(CategoryList):
         category = get_object_or_404(Category, slug=slug)
 
         if category.get_children().exists():
-            # Если у категории есть дочерние категории, возвращаем их
             return category.get_children()
         else:
-            # Иначе возвращаем товары для данной категории
             category = Category.objects.get(slug=slug)
-            print(category)
             products = Product.objects.filter(categories=category)
             return products
 
@@ -32,11 +26,10 @@ class CustomCategoryList(CategoryList):
         try:
             queryset = self.get_queryset()
             if hasattr(queryset, 'model') and queryset.model == Product:
-                # Если это набор товаров, используем соответствующий сериализатор для товаров
                 serializer = CustomBaseProductSerializer(queryset, many=True, context={'request': request})
             else:
-                # Если это набор категорий, используем соответствующий сериализатор для категорий
                 serializer = CustomCategorySerializer(queryset, many=True, context={'request': request})
             return Response(serializer.data)
         except Http404:
             return Response({"detail": "Категория не найдена"}, status=status.HTTP_404_NOT_FOUND)
+
